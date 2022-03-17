9c3f02d855296039da6acaff313c5612b5d29a3e
IG <iikim2511@gmail.com>
2022-03-17 14:06:37 -0400

    before--fetch data

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 let swiper = new Swiper(".home-slider", {
     loop: true,
     grabCursor: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
